8d68577e49c48d71f24017be8584917e3a00b4ad
YipengSong <yipeng.song@hotmail.com>
2022-09-06 16:48:47 -0600

    Update cosa2.R

--- R/cosa2.R
+++ R/cosa2.R
@@ -177,9 +177,8 @@ cosa2 <- function(X, lX = NULL, targ = NULL, targ2 = NULL, knear = sqrt(nrow(X))
   # Preparing the Data for Fortran (including missings):
   X_class <- class(X)
   cat("X_class", X_class)
-  if (length(X_class)<=1 & X_class == "data.frame") {
-    X <- sapply(X, function(x) as.numeric(x))
-  }
+  X <- sapply(X, function(x) as.numeric(x))
+  print(X_class)
                 
   if (missing(xmiss) && !is.null(attr(X, "xmiss"))) {
     xmisst <- attr(X, "xmiss")
